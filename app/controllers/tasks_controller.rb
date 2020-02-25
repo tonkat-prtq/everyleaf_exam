f5f3e156ea4b49e96d9449a90441e606d35a862b
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-02-25 20:49:27 +0900

    Add: コントローラにshowアクションとbefore_actionを記述

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i(show) 
   def index
     @tasks = Task.all
   end
@@ -20,6 +21,10 @@ class TasksController < ApplicationController
   end
 
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:name, :content)
