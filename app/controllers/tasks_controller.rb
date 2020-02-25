f226cfde1672b0acef23c02dd6dba9d4df71491e
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-02-25 10:43:28 +0900

    #4 Add:rootをtasks#indexに設定、それに伴いcreateアクション発火後のredirect先を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,7 +10,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to tasks_path, flash: {success: "タスクを登録しました"}
+      redirect_to root_path, flash: {success: "タスクを登録しました"}
     else
       render :new
     end
