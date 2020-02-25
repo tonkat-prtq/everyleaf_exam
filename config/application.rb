960d70c35d7308bfedd2b151480e95121c56c77a
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-02-25 10:09:43 +0900

    #4 Add:存在しないロケールを書いてもエラーを発生させないように

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module EveryleafExam
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    I18n.enforce_available_locales = false
     config.i18n.default_locale = :ja
     config.load_defaults 5.2
     config.generators do |g|
