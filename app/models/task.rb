99bb3dd3f70edbbbeace8c72a56f809918b31fea
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-02-26 09:14:05 +0900

    #5 Add:taskのバリデーション追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: {maximum: 140}
+  validates :content, presence: true
+  validates :content, length: {maximum: 300}
 end
