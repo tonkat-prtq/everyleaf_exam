5b0ddc5536c65eea3a809a672cc59bbfa54a0c88
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-02-29 17:26:17 +0900

    #8 Fix:日本語化の設定をinitializers/locale.rbに移行

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module EveryleafExam
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     I18n.enforce_available_locales = false
-    config.i18n.default_locale = :ja
     config.load_defaults 5.2
     config.generators do |g|
       g.assets false
