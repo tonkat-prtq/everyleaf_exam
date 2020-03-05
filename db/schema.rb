5cdc22e0142b08d3c9479bd04f6d4fba75780058
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-03-05 17:46:00 +0900

    #11 Add:tasksテーブルのname,contentにnull false制約追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_133226) do
+ActiveRecord::Schema.define(version: 2020_03_05_080240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
-    t.text "content"
+    t.string "name", null: false
+    t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
