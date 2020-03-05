ef3625e71a521e72a3abebc9e524d6de46e0d9fa
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-03-05 17:38:28 +0900

    #9 Add:投稿日時の降順に並び替え、そのSystem Spec

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -12,6 +12,16 @@ RSpec.describe 'Tasks', type: :system do
         expect(page).to have_content 'TEST_CONTENT'
       end
     end
+
+    context '複数のタスクを作成した場合' do
+      it 'タスクが作成日時の降順に並んでいること' do
+        new_task = create(:task, name: 'new_task')
+        visit tasks_path
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content 'new_task'
+        expect(task_list[1]).to have_content 'TEST_TASK'
+      end
+    end
   end
 
   describe 'タスク登録画面' do
@@ -25,10 +35,10 @@ RSpec.describe 'Tasks', type: :system do
         # 2.ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
         fill_in 'タスク名', with: "TEST_TASK"
         # 3.ここに「タスク詳細」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
-        fill_in 'タスク詳細', with: "TEST_CONTENT"
+        fill_in '内容', with: "TEST_CONTENT"
         # 「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）
         # 4.「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
-        click_button 'Create Task'
+        click_button '登録する'
         # clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
         # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
         # 5.タスク詳細ページに、テストコードで作成したはずのデータ（記述）がhave_contentされているか（含まれているか）を確認（期待）するコードを書く
