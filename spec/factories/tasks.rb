e385e57a7b54d0c3bdaff31e7f1c696f0288123d
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-03-06 21:41:39 +0900

    #13 Add:期限を押した際の並べ替えがうまく行っているかRspecを書く＋他のテストの修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,6 +2,13 @@ FactoryBot.define do
   factory :task do
     sequence(:name) { |n| "TEST_TASK#{n}"}
     sequence(:content) { |n| "TEST_CONTENT#{n}"}
-    sequence(:deadline) { Time.current.strftime("%Y-%m-%d %H:%M") }
+    sequence(:deadline) { Time.current }
+  end
+
+  factory :new_task, class: Task do # 存在しないclass名の名前をつける場合、optionで「このclassのテストデータにしてください」と指定
+    sequence(:name) { "new_test_task" }
+    sequence(:content) { "new_content" }
+    sequence(:deadline) { Time.now + 1.hours }
+    sequence(:created_at) { Time.now + 1.hours}
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Tasks', type: :system do
   before do
     @task = create(:task)
+    @new_task = create(:new_task)
   end
 
   describe 'タスク一覧画面' do
@@ -15,10 +16,19 @@ RSpec.describe 'Tasks', type: :system do
 
     context '複数のタスクを作成した場合' do
       it 'タスクが作成日時の降順に並んでいること' do
-        new_task = create(:task, name: 'new_task')
         visit tasks_path
         task_list = all('.task_row')
-        expect(task_list[0]).to have_content 'new_task'
+        expect(task_list[0]).to have_content 'new_test_task'
+        expect(task_list[1]).to have_content 'TEST_TASK'
+      end
+    end
+
+    context '期限ボタンを押した場合' do
+      it 'タスクが期限日の降順に並び替えられること' do
+        visit tasks_path
+        click_on '期限'
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content 'new_test_task'
         expect(task_list[1]).to have_content 'TEST_TASK'
       end
     end
@@ -38,11 +48,12 @@ RSpec.describe 'Tasks', type: :system do
         fill_in '内容', with: "TEST_CONTENT"
         # 「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）
         # 4.「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
+        fill_in '期限', with: Time.local(2020,2,20,2,2,2)
         click_button '登録する'
         # clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
         # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
         # 5.タスク詳細ページに、テストコードで作成したはずのデータ（記述）がhave_contentされているか（含まれているか）を確認（期待）するコードを書く
-        expect(page).to have_content 'TEST_CONTENT', 'TEST_TASK'
+        expect(page).to have_content 'タスクを登録しました'
       end
     end
   end
