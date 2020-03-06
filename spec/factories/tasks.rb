314605f2543b77416f58136a8b27aed672543883
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-03-06 18:29:47 +0900

    #13 Add:factoryBotで作成されるデータにdeadline

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :task do
     sequence(:name) { |n| "TEST_TASK#{n}"}
     sequence(:content) { |n| "TEST_CONTENT#{n}"}
+    sequence(:deadline) { Time.current.strftime("%Y-%m-%d %H:%M") }
   end
 end
