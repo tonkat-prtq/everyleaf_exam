0844112f92f6c78cb4e84618131e0cfb4324f955
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-03-09 09:07:24 +0900

    #14 Add:statusカラムのバリデーション

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,9 @@
 class Task < ApplicationRecord
   validates :name, presence: true
-  validates :name, length: {maximum: 140}
+  validates :name, length: {maximum: 40}
   validates :content, presence: true
   validates :content, length: {maximum: 300}
   validates :deadline, presence: true
+  validates :status, presence: true
+  validates :status, length: {maximum: 10}
 end
