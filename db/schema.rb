c50bf001a01a8f50cb646985383a1b5bd6f0798a
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-03-09 09:07:08 +0900

    #14 Add:Statusカラムを追加、migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_081124) do
+ActiveRecord::Schema.define(version: 2020_03_08_053525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_03_06_081124) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deadline", null: false
+    t.string "status", default: "未入力", null: false
   end
 
 end
