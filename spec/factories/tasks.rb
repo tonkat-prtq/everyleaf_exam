88ceb55387e126a62551e929294a0684b9c03577
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-03-13 21:53:33 +0900

    #14 Add:検索機能のモデルのテスト

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,13 +2,23 @@ FactoryBot.define do
   factory :task do
     sequence(:name) { |n| "TEST_TASK#{n}"}
     sequence(:content) { |n| "TEST_CONTENT#{n}"}
+    sequence(:status) { "未着手" }
     sequence(:deadline) { Time.current }
   end
 
   factory :new_task, class: Task do # 存在しないclass名の名前をつける場合、optionで「このclassのテストデータにしてください」と指定
     sequence(:name) { "new_test_task" }
     sequence(:content) { "new_content" }
+    sequence(:status) { "着手" }
     sequence(:deadline) { Time.now + 1.hours }
     sequence(:created_at) { Time.now + 1.hours}
+  end
+
+  factory :completed_task, class: Task do
+    sequence(:name) { "completed_task" }
+    sequence(:content) { "completed!"}
+    sequence(:status) { "完了" }
+    sequence(:deadline) { Time.now - 1.days }
+    sequence(:created_at) { Time.now - 2.days }
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe 'Tasks', type: :model do
   before do
     @task = build(:task)
+    @new_task = build(:new_task)
+    @completed_task = build(:completed_task)
   end
 
   describe 'taskのバリデーション' do
@@ -25,6 +27,52 @@ RSpec.describe 'Tasks', type: :model do
       expect(@task.valid?).to eq(false)
     end
 
+  end
+
+  describe '検索のテスト' do
+    before do
+      @new_task = create(:new_task)
+      @task = create(:task)
+      @completed_task = create(:completed_task)
+    end
+    
+    describe 'タスク名のみの検索' do
+      it '検索したタスク名が入っている' do
+        expect(Task.search_with_name("new_test_task")).to include(@new_task)
+      end
+
+      it 'タスク名にマッチしない場合はNG' do
+        expect(Task.search_with_name("no_match")).to_not include(@new_task, @task, @completed_task)
+      end
+    end
+
+    describe 'ステータスのみの検索' do
+      it '検索したステータスのみ表示される' do
+        expect(Task.search_with_status("着手")).to include(@new_task)
+      end
+
+      it 'ステータス名にマッチしない場合はNG' do
+        expect(Task.search_with_status("着手")).to_not include(@task, @completed_task)
+      end
+    end
+
+    describe 'タスク名とステータス名両方での検索' do
+      it 'どちらの検索条件にも引っかかるタスクがマッチする' do
+        expect(Task.search_with_name("new_test_task").search_with_status("着手")).to include(@new_task)
+      end
+
+      it 'タスク名のみマッチしてもステータスがマッチしなければ検索結果は得られない' do
+        expect(Task.search_with_name("new_test_task").search_with_status("未着手")).to_not include(@new_task)
+      end
+
+      it 'ステータスのみマッチしてもタスク名がマッチしなければ検索結果は得られない' do
+        expect(Task.search_with_name("qwerty").search_with_status("着手")).to_not include(@new_task)
+      end
+
+      it '両方にマッチするタスクがない場合、何も得られない' do
+        expect(Task.search_with_name("qwerty").search_with_status("qwerty")).to_not include(@task, @new_task, @completed_task)
+      end
+    end
   end
 end
 
