6792b12e36462169e64bbdd2479823396cee52aa
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-03-17 09:52:01 +0900

    #14 Add:検索のSystem Specを書く

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -12,21 +12,22 @@ RSpec.describe 'Tasks', type: :system do
   end
 
   describe 'タスク一覧画面' do
+    before do
+      visit tasks_path
+    end
 
     let(:set_task){
       task_list = all('.task_row')
     }
 
     context 'タスクを作成した場合' do
-      it '作成済みのタスクが表示されること' do
-        visit root_path
+      example '作成済みのタスクが表示されること' do
         expect(page).to have_text 'TEST_CONTENT'
       end
     end
 
     context '複数のタスクを作成した場合' do
-      it 'タスクが作成日時の降順に並んでいること' do
-        visit tasks_path
+      example 'タスクが作成日時の降順に並んでいること' do
         set_task
         within('tbody') do
           expect(page).to have_text /.*new_test_task.*TEST_TASK.*/m
@@ -36,8 +37,7 @@ RSpec.describe 'Tasks', type: :system do
     end
 
     context '期限ボタンを押した場合' do
-      it 'タスクが期限日の降順に並び替えられること' do
-        visit tasks_path
+      example 'タスクが期限日の降順に並び替えられること' do
         click_on '期限'
         set_task
         within('tbody') do
@@ -46,11 +46,63 @@ RSpec.describe 'Tasks', type: :system do
         end
       end
     end
+
+    describe 'タスクの検索機能' do
+
+      context 'タスク名とステータス名の両方で検索した場合' do
+        example 'マッチしたタスクのみが表示される' do
+          fill_in 'タスク名で検索', with: "TEST_TASK"
+          # find("option[value='未着手']").select_option
+          select '未着手', from: :status
+          click_button '検索'
+          expect(page).to have_text "TEST_TASK", "未着手"
+        end
+      end
+
+      context 'タスク名のみで検索した場合' do
+        before do
+          fill_in 'タスク名で検索', with: "TEST_TASK"
+          click_button '検索'
+        end
+
+        # it_behaves_like 'マッチしたタスクのみが表示される'
+
+        example 'マッチしたタスクのみが表示される' do
+          expect(page).to have_text "TEST_TASK"
+        end
+
+        example 'マッチしないタスクは表示されない' do
+          expect(page).to have_no_text "new_test_task"
+        end
+      end
+
+      context 'ステータスのみで検索した場合' do
+        before do
+          select '未着手', from: :status
+          click_button '検索'
+        end
+
+        example 'マッチしたタスクのみが表示される' do
+          expect(page).to have_text "未着手"
+        end
+
+        example 'マッチしないタスクは表示されない' do
+          within ('tbody') do
+            expect(page).to have_no_text /.*^着手$.*/m
+          end
+        end
+      end
+
+      example '検索フォームに何も入れなかった場合全てのタスクが表示される' do
+        click_button '検索'
+        expect(page).to have_text /.*new_test_task.*TEST_TASK.*/m
+      end
+    end
   end
 
   describe 'タスク登録画面' do
     context '必要項目を入力して、createボタンを押した場合' do
-      it 'データが保存されること' do
+      example 'データが保存されること' do
         # new_task_pathにvisitする（タスク登録ページに遷移する）
         # 1.ここにnew_task_pathにvisitする処理を書く
         visit new_task_path
@@ -74,7 +126,7 @@ RSpec.describe 'Tasks', type: :system do
 
   describe 'タスク詳細画面' do
     context '任意のタスク詳細画面に遷移した場合' do
-      it '該当タスクの内容が表示されたページに遷移すること' do
+      example '該当タスクの内容が表示されたページに遷移すること' do
         visit task_path(@task.id)
         expect(page).to have_text "#{@task.name}", "#{@task.content}"
       end
