c7af6c1d8620ff58f11a9a381e63b58a63e66a64
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-03-18 10:26:47 +0900

    #15 Add:priorityカラムの作成

---
+++ db/migrate/20200317012234_add_priority_to_tasks.rb
@@ -0,0 +1,10 @@
+class AddPriorityToTasks < ActiveRecord::Migration[5.2]
+  def up
+    add_column :tasks, :priority, :integer
+    change_column :tasks, :priority, :integer, null: false, default: 0
+  end
+
+  def down
+  remove_column :tasks, :priority
+  end
+end
