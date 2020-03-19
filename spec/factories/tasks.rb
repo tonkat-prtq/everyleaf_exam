a0cde49df30d2c2c4e16e7e48b59e602dec8daf8
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-03-19 21:09:25 +0900

    #15 Fix:priorityカラム追加に伴い、factoryBotの内容を編集

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,14 +2,16 @@ FactoryBot.define do
   factory :task do
     sequence(:name) { |n| "TEST_TASK#{n}"}
     sequence(:content) { |n| "TEST_CONTENT#{n}"}
-    sequence(:status) { "未着手" }
+    sequence(:status) { :waiting }
+    sequence(:priority) { :high }
     sequence(:deadline) { Time.current }
   end
 
   factory :new_task, class: Task do # 存在しないclass名の名前をつける場合、optionで「このclassのテストデータにしてください」と指定
     sequence(:name) { "new_test_task" }
     sequence(:content) { "new_content" }
-    sequence(:status) { "着手" }
+    sequence(:status) { :working }
+    sequence(:priority) { :medium }
     sequence(:deadline) { Time.now + 1.hours }
     sequence(:created_at) { Time.now + 1.hours}
   end
@@ -17,7 +19,8 @@ FactoryBot.define do
   factory :completed_task, class: Task do
     sequence(:name) { "completed_task" }
     sequence(:content) { "completed!"}
-    sequence(:status) { "完了" }
+    sequence(:status) { :completed }
+    sequence(:priority) { :low }
     sequence(:deadline) { Time.now - 1.days }
     sequence(:created_at) { Time.now - 2.days }
   end
