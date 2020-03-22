43bcf2901013e0b6b4316d52de3ba59936b9c242
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-03-22 21:00:11 +0900

    #19 Add:バリデーション、emailにindex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  before_validation { email.downcase! }
+  validates :email, presence: true, length: { maximum: 255 }
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :email, uniqueness: true
 end
