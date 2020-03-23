c107ebc5b56794c40584cd7ba1e83b39c7ed6035
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-03-23 20:27:17 +0900

    #19 Add:Userの作成機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :tasks
   root to: 'tasks#index'
+  resources :users, only: [:new, :create]
 end
