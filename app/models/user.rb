d5b198241440c467c035f16ea1c68d3982ab4fb0
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-03-25 21:41:58 +0900

    #21 Add:dependent destroyをuserに追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
   validates :name, presence: true, length: { maximum: 50 }
   before_validation { email.downcase! }
   validates :email, presence: true, length: { maximum: 255 }
