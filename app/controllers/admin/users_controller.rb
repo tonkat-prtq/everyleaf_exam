9e50b9011062aee0b3f598134c5f8ac660c191a9
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-03-25 21:02:17 +0900

    #21 Add:アクション追加、権限の有無を確認

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,21 +1,42 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :admin_check
+  
   def index
     @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
   end
 
   def show
+
   end
 
   def edit
+
   end
 
   def update
+    if params[:back]
+      render :edit
+    else
+      if @user.update(user_params)
+        redirect_to user_path(@user.id), flash: {success: "ユーザー情報を更新しました"}
+      else
+        render :edit
+      end
+    end
   end
 
   def destroy
@@ -27,4 +48,14 @@ class Admin::UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
   end
 
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def admin_check
+    if current_user.admin
+    else
+      redirect_to user_path(current_user.id)
+    end
+  end
 end
