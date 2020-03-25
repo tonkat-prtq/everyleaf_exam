c39ae3786e361ab73a6518d939248ac0d3a5138a
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-03-25 20:45:26 +0900

    #21 Add:indexページ仮実装、それに伴い辞書ファイルの編集

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,30 @@
 class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+
 end
