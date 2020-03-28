d0750f7984135ad7a8cc391b31e83df72d91ec17
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-03-28 17:37:34 +0900

    #22 Add:FactoryBotでユーザーを作るように

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:name) { |n| "sample_user#{n}" }
+    sequence(:email) { |n| "sample#{n}@example.com" }
+    sequence(:password) { "password" }
+    sequence(:admin) { false }
+  end
+
+  factory :admin_user, class: User do
+    sequence(:name) { "admin#" }
+    sequence(:email) { "admin@example.com" }
+    sequence(:password) { "password" }
+    sequence(:admin) { true }
   end
 end
