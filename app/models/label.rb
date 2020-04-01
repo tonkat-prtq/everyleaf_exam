ff7a3b04dbe8401a6f3e122f14873a4a6c201582
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-04-01 21:15:05 +0900

    #25 Fix:スペルミス修正

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,6 +1,6 @@
 class Label < ApplicationRecord
-  has_many: :labelings, dependent: :destroy
-  has_many: :tasks, through: :labelings
+  has_many :labelings, dependent: :destroy
+  has_many :tasks, through: :labelings
 
   validates :name, presence: true
   validates :name, length: {maximum: 30}
