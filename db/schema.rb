15b53bbdc78dd8a7eac900a232f3ef4126b29451
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-04-01 21:10:01 +0900

    #25 Add:アソシエーションの設定、バリデーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_115729) do
+ActiveRecord::Schema.define(version: 2020_04_01_120819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2020_04_01_115729) do
   end
 
   create_table "labels", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
