39637227031a7c2aef0441c11ca32cc9ab74ecca
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-04-05 17:36:31 +0900

    Fix:labelingに複合インデックスと複数カラムのユニーク制約を追加し1つのタスクに複数個の同じラベルがつかないように

--- app/models/labeling.rb
+++ app/models/labeling.rb
@@ -1,4 +1,5 @@
 class Labeling < ApplicationRecord
   belongs_to :task
   belongs_to :label
+  validates :id, uniqueness: { scope: [:task_id, :label_id] }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_120819) do
+ActiveRecord::Schema.define(version: 2020_04_05_082802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_04_01_120819) do
     t.bigint "label_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["label_id", "task_id"], name: "index_labelings_on_label_id_and_task_id", unique: true
     t.index ["label_id"], name: "index_labelings_on_label_id"
     t.index ["task_id"], name: "index_labelings_on_task_id"
   end
